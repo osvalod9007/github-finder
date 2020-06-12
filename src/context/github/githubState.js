e4935ff867f07b663fac0fa4e6ed956aa7c18a25
gsi-osvaldolopez <osvaldo.lopez@generalsoftwareinc.com>
2020-06-12 15:44:10 -0500

    if env is production or not

--- src/context/github/githubState.js
+++ src/context/github/githubState.js
@@ -10,6 +10,17 @@ import {
   SET_LOADING,
 } from '../types';
 
+let githunClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'producction') {
+  githunClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githunClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 const GithubState = (props) => {
   const initialState = {
     users: [],
@@ -25,7 +36,7 @@ const GithubState = (props) => {
     setLoading();
 
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${githunClientId}&client_secret=${githubClientSecret}`
     );
     dispatch({
       type: SEARCH_USERS,
@@ -38,7 +49,7 @@ const GithubState = (props) => {
     setLoading();
 
     const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}?client_id=${githunClientId}&client_secret=${githubClientSecret}`
     );
     dispatch({
       type: GET_USER,
@@ -51,7 +62,7 @@ const GithubState = (props) => {
     setLoading();
 
     const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githunClientId}&client_secret=${githubClientSecret}`
     );
 
     dispatch({
